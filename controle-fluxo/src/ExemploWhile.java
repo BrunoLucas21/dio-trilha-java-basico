2a01b9291d4e2455724d6fd2cc57936c49de3a11
BrunoLucas21 <brunolucasafreire@gmail.com>
2025-04-09 10:47:32 -0300

    entendo o while

---
+++ controle-fluxo/src/ExemploWhile.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+  public static void main(String[] args) {
+    double mesada = 50.0;
+    while(mesada > 0) {
+      Double valorDoce = valorAleatorio();
+      if (valorDoce > mesada) {
+        valorDoce = mesada;
+      }
+      System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho.");
+      mesada -= valorDoce;
+    }
+    System.out.println("Mesada: " + mesada + " Acabou!");
+    System.out.println("Carrinho cheio!");
+  }
+
+  private static double valorAleatorio() {
+    return ThreadLocalRandom.current().nextDouble(2, 8);
+  }
+}
